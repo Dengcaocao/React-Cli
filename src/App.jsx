57367252c216490c5a6fd5062b20f3b01b7169eb
dw <1536698087@qq.com>
2023-07-31 11:10:07 +0800

    添加路由页面

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/home/home'
+import About from './pages/about/about'
 
 const App = () => {
   return (
     <>
-      <h1>Hello World888</h1>
-      <Home />
+      <h1>App</h1>
+      <ul>
+        <li>
+          <Link to='/home'>首页</Link>
+        </li>
+        <li>
+        <Link to='/about'>关于</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </>
   )
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
 // 渲染你的 React 组件
 const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(
+  <Router>
+    <App />
+  </Router>
+)
